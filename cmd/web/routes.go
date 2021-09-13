8f173836c693c97dd89d1fb7f6f687a27fc82d5c
Terry Kang <89037976+tkangsquare@users.noreply.github.com>
2021-09-13 09:10:27 -0700

    update routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,16 +22,5 @@ func (app *application) routes() http.Handler {
 	mux.Get("/giftcards/:id/deactivate", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.deactivateAGiftCardView))
 	mux.Get("/giftcards/:id/load", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.loadAGiftCardView))
 
-	mux.Get("/oauth/square/login", dynamicMiddleware.ThenFunc(app.squareOauthLogin))
-	mux.Get("/oauth/square/callback", dynamicMiddleware.ThenFunc(app.squareOauthCallback))
-
-	mux.Get("/giftcards/:id", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.showAGiftCardView))
-	mux.Post("/giftcards", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.createAGiftCard))
-
-	mux.Post("/giftcards/:id/send", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.sendAGiftCard))
-	mux.Post("/giftcards/:id/activate", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.activateAGiftCard))
-	mux.Post("/giftcards/:id/deactivate", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.deactivateAGiftCard))
-	mux.Post("/giftcards/:id/load", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.loadAGiftCard))
-
 	return standardMiddleware.Then(mux)
 }
